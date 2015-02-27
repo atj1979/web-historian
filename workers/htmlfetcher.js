f8efc2f71e25cea6ba135f1053d8fa2fe524d434
atj1979 <atj1979@gmail.com>
2015-02-26 17:28:46 -0800

    Add   Worker function works now.  need to add CRON.

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,11 +1,25 @@
 var helpers = require('../helpers/archive-helpers.js');
 var request = require('request');
+var fs = require('fs');
 // eventually, you'll have some code here that uses the code in `archive-helpers.js`
 // to actually download the urls you want to download.
 helpers.readListOfUrls(function (list) {
-  list.forEach(function(){
+  list.forEach(function(url){
+    console.log(url);
     //do some stuff
-  })
+    request.get({url: 'http://'+url
+      // progress: function(res) {
+      //   console.log(res);
+      //   return;
+      // }
+    },
+    helpers.paths.archivedSites + '/' + url,
+    function(err, res){
+      console.log(res);
+      fs.writeFile(helpers.paths.archivedSites + '/' + url);
+      return;
+    });
+  });
 
 
   // last thing we do is delete the list

7f2c8834329035394245227af20fae748f2d5320
atj1979 <atj1979@gmail.com>
2015-02-26 20:15:01 -0800

    completed basic requirements

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,30 +1,49 @@
 var helpers = require('../helpers/archive-helpers.js');
 var request = require('request');
 var fs = require('fs');
+var CronJob = require('cron').CronJob;
 // eventually, you'll have some code here that uses the code in `archive-helpers.js`
 // to actually download the urls you want to download.
-helpers.readListOfUrls(function (list) {
-  list.forEach(function(url){
-    console.log(url);
-    //do some stuff
-    request.get({url: 'http://'+url
-      // progress: function(res) {
-      //   console.log(res);
-      //   return;
-      // }
-    },
-    helpers.paths.archivedSites + '/' + url,
-    function(err, res){
-      console.log(res);
-      fs.writeFile(helpers.paths.archivedSites + '/' + url);
-      return;
+var archive = function(){
+  console.log('archeeveing');
+  helpers.readListOfUrls(function (list) {
+    list.forEach(function(url){
+      // console.log(url);
+      //do some stuff
+      url !== '' && request.get({url: 'http://'+url
+        // progress: function(res) {
+        //   console.log(res);
+        //   return;
+        // }
+      },
+      helpers.paths.archivedSites + '/' + url,
+      function(err, res){
+        if (err){
+          console.log(err);
+        }
+        // console.log(res);
+        fs.writeFile(helpers.paths.archivedSites + '/' + url, res.body);
+        return;
+      });
     });
+
+
+    // last thing we do is delete the list
   });
+  // check the list
+  // get the site
+  // put the site in a file
+}
 
 
-  // last thing we do is delete the list
-});
-// check the list
-// get the site
-// put the site in a file
-
+var job = new CronJob('* */1 * * * *', function(){
+    // Runs every weekday (Monday through Friday)
+    // at 11:30:00 AM. It does not run on Saturday
+    // or Sunday.
+    archive();
+  }, function () {
+    // This function is executed when the job stops
+  },
+  true /* Start the job right now */,
+  "America/Los_Angeles" /* Time zone of this job. */
+);
